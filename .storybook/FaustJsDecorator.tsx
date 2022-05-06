6ab04bbf0993bc452705507dc938033ac3b6be54
Maurice Tadros <maurice@bowriverstudio.com>
2022-05-06 10:10:25 -0600

    Moved FaustProvider to Storybook

---
+++ .storybook/FaustJsDecorator.tsx
@@ -0,0 +1,25 @@
+import "../src/faust.config";
+
+import * as React from "react";
+
+import { StoryContext, StoryFn } from "@storybook/addons";
+
+import { client } from "../src/client";
+import { FaustProvider } from "@faustjs/next";
+
+export const FaustJsDecorator = (
+  Story: StoryFn<any>,
+  context: StoryContext
+) => {
+  return (
+    <>
+      <FaustProvider
+        client={client}
+        pageProps={{ __AUTH_CLIENT_CACHE_PROP: "", __CLIENT_CACHE_PROP: "" }}
+      >
+        <Story {...context} />
+      </FaustProvider>
+      s
+    </>
+  );
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 import { addDecorator } from "@storybook/react";
 import { withConsole } from "@storybook/addon-console";
+import { FaustJsDecorator } from "./FaustJsDecorator";
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+
+export const decorators = [FaustJsDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
